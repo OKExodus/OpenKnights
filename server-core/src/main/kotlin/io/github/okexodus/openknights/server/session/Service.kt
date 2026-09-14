c9b4623c390fa8d8a48110dbf0caa12df8402837
OKExodus <328171079+OKExodus@users.noreply.github.com>
2026-09-13 19:26:01 -0500

    Check the item-Fortify bonus policy at service start

--- server-core/src/main/kotlin/io/github/okexodus/openknights/server/session/Service.kt
+++ server-core/src/main/kotlin/io/github/okexodus/openknights/server/session/Service.kt
@@ -207,7 +207,7 @@ class Service(
             val service = Service(driver, log, clock, tables, data, auth, world, select, root, generation)
             service.acquisitionPolicy = checkAcquisitionPolicy(data.document("policies/acquisition-rng.json"))
             service.evolutionTestPolicy = io.github.okexodus.openknights.server.game.HeroEvolution.checkTestPolicy(data.policy("evolution")) as JObj?
-            service.fortifyBonusPolicy = data.policy("fortify-bonus")
+            service.fortifyBonusPolicy = io.github.okexodus.openknights.server.game.ItemFortify.checkBonusPolicy(data.policy("fortify-bonus"))
             service.powerUpPolicy = io.github.okexodus.openknights.server.game.HeroPowerUp.checkPolicy(data.policy("power-up")) as JObj?
             service.ascensionPolicy = io.github.okexodus.openknights.server.game.HeroAscension.checkMaterialPolicy(data.policy("ascension")) as JObj?
             io.github.okexodus.openknights.server.game.Events.setActive(io.github.okexodus.openknights.server.game.Events.releaseEvents(data))
